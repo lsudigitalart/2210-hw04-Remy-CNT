daf27167de0ae3e3cbba734c30891b9f2e2e67b1
Derick Ostrenko <dostrenko@lsu.edu>
2024-09-30 13:50:37 -0500

    timer

--- sketch.js
+++ sketch.js
@@ -4,13 +4,15 @@ var bally = 300;
 var ballSize = 40;
 var score = 0;
 var gameState = "L1"
+var timer = 10;
+
 function setup() {
   createCanvas (400, 400);
 }
 
 function draw() {
   background(220);
-  if(gameState = "L1"){
+  if(gameState == "L1"){
     levelOne();
   }
 if(gameState =="L2"){
@@ -19,9 +21,25 @@ if(gameState =="L2"){
 if(gameState == "L3"){
   levelThree();
 }
+  text("Timer: " + timer, width/2, 60);
+  text (("Score: " + score), width/2, 40);
 
-  text (("Score:" + score), width/2,40);
+  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+    timer --;
+  }
+
+  if(timer == 0) {
+    text("END GAME", width/2, 80)
+  }
+
+  print(timer);
+  
 }
+
+// let timer 10;
+
+// var seconds = 0;
+// var lastSecond = 0;
 
 function levelOne() {
   text ("Level 1", width/2, height-20);
@@ -34,7 +52,7 @@ function levelOne() {
 if(score>= 5){
   gameState = "L2"
 }
-  ellipse(ballx,bally,ballSize,ballSize);
+  ellipse(ballx,bally,ballSize);
   line(ballx,bally,mouseX,mouseY)
 } 
 
@@ -53,7 +71,7 @@ function levelTwo() {
 if(score>= 10){
   gameState = "L3";
 }
-  ellipse(ballx,bally,ballSize,ballSize);}
+  ellipse(ballx,bally,ballSize);}
    //line(ballx,bally,mouseX,mouseY)
 
    //end of level 2
@@ -72,7 +90,8 @@ if(score>= 10){
   if(score>= 15){
      background(random (255));
   }
-    ellipse(ballx,bally,ballSize,ballSize);}
+    ellipse(ballx,bally,ballSize);
+  }
      //line(ballx,bally,mouseX,mouseY)
   
      //end of level 3
